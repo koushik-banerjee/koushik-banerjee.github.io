60cf0a2b9391c3cb573b6f9262cdd832de993ec3
Koushik <banerjeekoushik04@gmail.com>
2025-06-20 14:44:04 +0530

    js commit

---
+++ js/file1.js
@@ -0,0 +1,51 @@
+// let studentName = "John";
+// let age = 20;
+// let promoted = true;
+// let score 
+// let city = null
+// console.log(studentName); // string
+// console.log(typeof studentName); // string
+// console.log(typeof age); // number
+// console.log(typeof promoted); // boolean
+// console.log(typeof score); // undefined
+// console.log(typeof city); // object
+
+// console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
+// console.log(typeof NaN); // number
+// console.log(typeof Infinity); // number
+
+
+
+// let a = "10"
+// let b = 10
+// console.log(a+b) // it return string 1010 it concatinates due to string expression +
+// console.log(a*b) // return number 100 it multiplies as string dont have expression like * <-this
+
+// const c1 = "lpu"
+// c1="MU"
+// console.log(c1) // error const value cannot be changed
+
+// var stname = "john"
+// console.log(stname);
+
+
+// let stname = "koushik"
+// console.log(`my name is ${stname}`)
+
+
+// let c = 10
+// console.log(c++)
+
+
+// let a = 10
+// let b=5
+// console.log(a>b) //returns true
+
+
+let a = "10"
+let b= 10
+console.log(a==b) // return true only value not type
+console.log(a===b) // return false checks both type and value
+
+
