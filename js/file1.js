6b883cd0e43f9859311711f836a13d06861e31e8
Koushik <banerjeekoushik04@gmail.com>
2025-06-23 14:22:54 +0530

    js commit

--- js/file1.js
+++ js/file1.js
@@ -1,51 +1,37 @@
-// let studentName = "John";
-// let age = 20;
-// let promoted = true;
-// let score 
+//Introduction to JavaScript
+// let studentName = "John"
+// let age = 20
+// let promoted = true
+// let score
 // let city = null
-// console.log(studentName); // string
-// console.log(typeof studentName); // string
-// console.log(typeof age); // number
-// console.log(typeof promoted); // boolean
-// console.log(typeof score); // undefined
-// console.log(typeof city); // object
+// console.log(studentName)
+// console.log(typeof studentName)
+// console.log(typeof age)
+// console.log(typeof promoted)
+// console.log(typeof score)
+// console.log(typeof city)
 
-// console.log(typeof null); // object
-// console.log(typeof undefined); // undefined
-// console.log(typeof NaN); // number
-// console.log(typeof Infinity); // number
-
-
+// let age = "10"
+// console.log(typeof age)
+// let ageNum = Number(age)
+// console.log(typeof ageNum)
 
 // let a = "10"
 // let b = 10
-// console.log(a+b) // it return string 1010 it concatinates due to string expression +
-// console.log(a*b) // return number 100 it multiplies as string dont have expression like * <-this
+// console.log(a+b)
 
-// const c1 = "lpu"
-// c1="MU"
-// console.log(c1) // error const value cannot be changed
+// let a = "10"
+// let b = 10
+// console.log(a*b)
 
-// var stname = "john"
-// console.log(stname);
+// const C1 = "LPU"
+// C1 = "MU"
+// console.log(C1)
 
+/*
+var stName = "John"
+console.log(stName)
+*/
 
-// let stname = "koushik"
-// console.log(`my name is ${stname}`)
-
-
-// let c = 10
-// console.log(c++)
-
-
-// let a = 10
-// let b=5
-// console.log(a>b) //returns true
-
-
-let a = "10"
-let b= 10
-console.log(a==b) // return true only value not type
-console.log(a===b) // return false checks both type and value
-
-
+let stName = "Kushboo"
+console.log(`My name is ${stName}`)
---
+++ js/file9.js
@@ -0,0 +1,19 @@
+// arrays and methods
+// const score = [2, 1, 7, 5, 3];
+// console.log(score)
+// console.log(score[0])
+// score.push(9)
+// console.log(score)
+// console.log(score.length)
+// const newScore = [...score,9]
+// console.log(newScore)
+// shopping cart app
+
+
+const score =[2,1,34,5,6,27];
+
+
+// score.forEach(value,index,array)((val)=>{})
+score.forEach((user)=>{
+    console.log(value);
+})
